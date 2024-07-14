d947ef32f6dd967adb03c8cc0ddcd055ca1bf53e
Alok jaiswal <72200826+alokjai@users.noreply.github.com>
2024-07-14 10:47:37 +0000

    conversionoperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,12 @@
+console.log("1" + 3);
+console.log(1 + "3");
+
+console.log("3" + "3" + 1); // 331
+console.log(1+2+"3"); // 33
+
+
+// console.log(+true); // 1
+
+// console.log(true+); // error
+
+console.log(+""); // 0 

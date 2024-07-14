a52ea71e4342d7f1f8cfa81258e9638084870d9b
Alok jaiswal <72200826+alokjai@users.noreply.github.com>
2024-07-14 07:31:02 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,15 @@
+
+"use strict"; // treat all js code newr version
+
+// number = 2;
+// Bigint
+// string = "";
+// boolean = true/false
+// null = standalone value, special type (empty)
+// undefined  jo define nahi kiya gaya hai
+
+// symbol= unique
+
+// object
+console.log(typeof null);  // output --> object 
+console.log(typeof undefined) //  output  --> undefined

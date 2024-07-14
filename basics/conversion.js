c292c8dd8e7fe1258e1e78ff122bc926c1eb1aae
Alok jaiswal <72200826+alokjai@users.noreply.github.com>
2024-07-14 08:01:25 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,36 @@
+let someval = "33";
+
+console.log(typeof someval);
+
+let convertval  = Number(someval);
+console.log(typeof convertval);
+
+//  "33" => 33 
+//  "33abc" => NaN // Not a Number
+//  true => 1; false => 0 
+
+
+let isloggedin = "" // ""/ "hello" / 788
+
+console.log(typeof isloggedin);
+
+let conLog = Boolean(isloggedin);
+console.log(conLog);
+console.log(typeof conLog);
+
+// 1 => true; 0 => false
+// "" => false
+// "alok" => true
+
+
+
+let someNum = 33 // ""/ "hello" / 788
+
+console.log(typeof someNum);
+
+let conStr = String(someNum);
+console.log(conStr);
+console.log(typeof conStr);
+
+
+
